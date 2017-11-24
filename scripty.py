1f939f8af44b57b704996775a35a1b27861d0a42
mongoosek <33965989+mongoosek@users.noreply.github.com>
2017-11-24 11:01:37 -0800

    Update and rename script.py to scripty.py

--- scripty.py
+++ scripty.py
@@ -6,5 +6,5 @@ driver.get("http://wwwadidas.com/yeezy")
 elem = driver.find_elements_by_xpath("//*[contains(text(), 'SIZE')]")
 while(len(elem)==0):
     elem = driver.find_elements_by_xpath("//*[contains(text(), 'SIZE')]")
-    print elem
-print "emailed"
+    print (elem)
+print ("emailed")
