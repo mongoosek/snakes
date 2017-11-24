bd38a0c9f87d5241eaa03073a28eb0058b9d0583
mongoosek <33965989+mongoosek@users.noreply.github.com>
2017-11-24 10:50:48 -0800

    Create script.py

---
+++ script.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Safari()
+driver.get("http://wwwadidas.com/yeezy")
+elem = driver.find_elements_by_xpath("//*[contains(text(), 'SIZE')]")
+while(len(elem)==0):
+    elem = driver.find_elements_by_xpath("//*[contains(text(), 'SIZE')]")
+    print elem
+print "emailed"
