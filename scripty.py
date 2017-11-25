0c375d7c87dba6cf02904e92a4fad7cf04fc71ef
mongoosek <33965989+mongoosek@users.noreply.github.com>
2017-11-24 22:38:34 -0800

    Update scripty.py

--- scripty.py
+++ scripty.py
@@ -18,7 +18,7 @@ output = p.stdout.read()
 print output
 
 
-driver = webdriver.Safari()
+driver = webdriver.Firefox()
 driver.get("http://www.adidas.com/yeezy")
 elem = driver.find_elements_by_xpath("//*[contains(text(), 'SIZE CHART')]")
 while(len(elem)==0):
