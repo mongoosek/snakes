d33031bf8ecb53146651de7d2fcb6e473b3be2be
mongoosek <33965989+mongoosek@users.noreply.github.com>
2017-11-24 22:37:32 -0800

    Update scripty.py

--- scripty.py
+++ scripty.py
@@ -1,10 +1,30 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import subprocess
+import requests
 
-driver = webdriver.Firefox()
-driver.get("http://wwwadidas.com/yeezy")
-elem = driver.find_elements_by_xpath("//*[contains(text(), 'SIZE')]")
+output = "undefined"
+
+def send_simple_message():
+    os.system("curl -s --user 'api:key-1d83a78fef75bfe2aa4f90449f464e69' \
+    https://api.mailgun.net/v3/sandboxd41db11df9e54aa8b9b9474fb6af27e0.mailgun.org/messages \
+        -F from='Mailgun Sandbox <mailgun@sandboxd41db11df9e54aa8b9b9474fb6af27e0.mailgun.org>' \
+        -F to='Rohan <rohandhesikan@gmail.com>' \
+        -F subject='We hit "+output+"' \
+        -F text='Log in ASAP'")
+p = subprocess.Popen(["hostname -I"], shell=True, stdout=subprocess.PIPE)
+output = p.stdout.read()
+print output
+
+
+driver = webdriver.Safari()
+driver.get("http://www.adidas.com/yeezy")
+elem = driver.find_elements_by_xpath("//*[contains(text(), 'SIZE CHART')]")
 while(len(elem)==0):
-    elem = driver.find_elements_by_xpath("//*[contains(text(), 'SIZE')]")
-    print (elem)
-print ("emailed")
+    elem = driver.find_elements_by_xpath("//*[contains(text(), 'SIZE CHART')]")
+send_simple_message()
+print 'detected'
+
+
+
